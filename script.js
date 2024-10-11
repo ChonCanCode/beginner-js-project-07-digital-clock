388b63414892639b6ba0a97304187c056c75324a
ChonCanCode <163328241+ChonCanCode@users.noreply.github.com>
2024-10-11 16:35:38 +0100

    2024.10.11

--- script.js
+++ script.js
@@ -2,19 +2,20 @@ window.addEventListener('load', calculateTime)
 
 function calculateTime(){
     let date = new Date();
-    let daynumber = date.getDay();
+    let dayNumber = date.getDay();
     let hour = date.getHours();
     let minute = date.getMinutes();
     let ampm = hour >= 12 ? 'PM' : 'AM';
     let dayNames = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT" ]
 
     hour = hour % 12;
-    hour = hour ? 12 : '12';
-    hour = hour < 10 ? '0' + minute : minute;
+    hour = hour === 0 ? '12' : hour;
+    hour = hour < 10 ? '0' + hour : hour;
 
-    document.getElementById("day").innerHTML = dayNames [dayNumber];
+    document.getElementById("day").innerHTML = dayNames[dayNumber];
     document.getElementById("hour").innerHTML = hour;
     document.getElementById("minute").innerHTML = minute;
     document.getElementById("ampm").innerHTML = ampm;
     setTimeout(calculateTime, 200);    
 }
+
